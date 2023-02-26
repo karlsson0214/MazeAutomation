a0debda09e6825f21ccc0852329e49ede64996f9
Karlsson0214 <karlsson0214@gmail.com>
2023-02-26 18:09:01 +0100

    save camera to png-file

---
+++ Assets/Scripts/SaveCamera.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveCamera : MonoBehaviour
+{
+    Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("pressed s");
+            SaveCameraView(cam);
+
+        }
+    }
+
+    void SaveCameraView(Camera cam)
+    {
+        RenderTexture screenTexture = new RenderTexture(Screen.width, Screen.height, 16);
+        cam.targetTexture = screenTexture;
+        RenderTexture.active = screenTexture;
+        cam.Render();
+        Texture2D renderedTexture = new Texture2D(Screen.width, Screen.height);
+        renderedTexture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        RenderTexture.active = null;
+        /*
+        for (int x = 0; x < Screen.width; x = x + 10)
+        {
+            Debug.Log(renderedTexture.GetPixel(x, x));
+        }
+        */
+
+        byte[] byteArray = renderedTexture.EncodeToPNG();
+        System.IO.File.WriteAllBytes(Application.dataPath + "/cameracapture.png", byteArray);
+        Debug.Log(Application.dataPath + "/cameracapture.png");
+    }
+}
