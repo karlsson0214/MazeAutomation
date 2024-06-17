281bfc00d134d7356c14b9143d753a16e34dd2f9
Karlsson0214 <karlsson0214@gmail.com>
2024-06-17 15:02:38 +0200

    first?

--- Assets/Scripts/SaveCamera.cs
+++ Assets/Scripts/SaveCamera.cs
@@ -31,12 +31,12 @@ public class SaveCamera : MonoBehaviour
         Texture2D renderedTexture = new Texture2D(Screen.width, Screen.height);
         renderedTexture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
         RenderTexture.active = null;
-        /*
+        
         for (int x = 0; x < Screen.width; x = x + 10)
         {
             Debug.Log(renderedTexture.GetPixel(x, x));
         }
-        */
+        
 
         byte[] byteArray = renderedTexture.EncodeToPNG();
         System.IO.File.WriteAllBytes(Application.dataPath + "/cameracapture.png", byteArray);
